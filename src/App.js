bb772db1f533cc3bf30a14525ab2344f4e1fe225
durga <durgaprasad230698@gmail.com>
2023-05-22 12:02:24 +0530

    Changes with single element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Single Element">
+      <div>
+        <h1>Creating Single Element</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Div means using single element.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <p>
+        paranthesis using for multiple statements
+      </p>
     </div>
   );
 }
-
 export default App;
